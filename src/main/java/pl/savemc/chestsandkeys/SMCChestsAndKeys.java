9ba0863beb54b84f868c4eb0141c076f05048523
Rollczi <ndejlich5@gmail.com>
2021-07-14 14:16:37 +0200

    initial commit

---
+++ src/main/java/pl/savemc/chestsandkeys/SMCChestsAndKeys.java
@@ -0,0 +1,17 @@
+package pl.savemc.chestsandkeys;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SMCChestsAndKeys extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
