41531f6bf9fe6aba303dfadb860e8cca448df510
Kamicjusz <kamicjusz.kontakt@gmail.com>
2021-07-17 12:18:37 +0200

    początek
    Zacząłem prace, postawiłem ItemBuildera, Managera, ChatUtilsa, stworzyłem przedmioty, rozpocząłem pisanie kodu pod dropienie kluczy

--- src/main/java/pl/savemc/chestsandkeys/SMCChestsAndKeys.java
+++ src/main/java/pl/savemc/chestsandkeys/SMCChestsAndKeys.java
@@ -1,17 +1,25 @@
 package pl.savemc.chestsandkeys;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import pl.savemc.chestsandkeys.Utils.ItemManager;
 
 public final class SMCChestsAndKeys extends JavaPlugin {
 
+    private ItemManager             itemManager;
+
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        this.itemManager = new ItemManager();
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public ItemManager getItemManager() {
+        return itemManager;
     }
 }
---
+++ src/main/java/pl/savemc/chestsandkeys/Utils/ChatUtils.java
@@ -0,0 +1,33 @@
+package pl.savemc.chestsandkeys.Utils;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class ChatUtils {
+
+    private ChatUtils(){}
+
+    public static String colored(String toColor){
+        return ChatColor.translateAlternateColorCodes('&', toColor);
+    }
+
+    public static List<String> colored(List<String> toColor) {
+        return toColor.stream()
+                .map(ChatUtils::colored)
+                .collect(Collectors.toList());
+    }
+
+    public static boolean sendMessage(Player player, String message){
+        player.sendMessage(colored(message));
+        return true;
+    }
+
+    public static boolean sendMessage(CommandSender commandSender, String message){
+        commandSender.sendMessage(colored(message));
+        return true;
+    }
+}
