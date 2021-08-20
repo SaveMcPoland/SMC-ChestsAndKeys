e6aabf895f5369901f92cc84e1fecd2e128e7434
Kamicjusz <kamicjusz.kontakt@gmail.com>
2021-08-20 23:27:30 +0200

    Classes of Chest and Keys added.

--- src/main/java/pl/savemc/chestsandkeys/objects/Key.java
+++ src/main/java/pl/savemc/chestsandkeys/objects/Key.java
@@ -5,31 +5,23 @@ import org.bukkit.inventory.ItemStack;
 import pl.savemc.chestsandkeys.utils.ItemBuilder;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class Chest {
+public class Key {
 
-    private ItemBuilder chest;
+    private ItemBuilder key;
     private String name;
     private List<String> lore;
-    private List<ItemStack> drops;
-    private Key key;
 
-    public Chest(String name, List<String> lore){
-        this.chest = new ItemBuilder(Material.CHEST);
-        drops = new ArrayList<>();
+    public Key(String name, List<String> lore){
+        this.key = new ItemBuilder(Material.TRIPWIRE_HOOK);
         lore = new ArrayList<>();
         this.name = name;
         this.lore = lore;
-        this.key = new Key("test", Arrays.asList("1","2"));
-        chest.setName(name, true);
-        chest.setLore(lore);
+        key.setName(name, true);
+        key.setLore(lore);
     }
 
-    public void setDrops(List<ItemStack> drops) {
-        this.drops = drops;
-    }
 
     public void setLore(List<String> lore) {
         this.lore = lore;
@@ -47,15 +39,7 @@ public class Chest {
         return name;
     }
 
-    public List<ItemStack> getDrops() {
-        return drops;
-    }
-
     public ItemStack getItem(){
-        return chest.getItem();
-    }
-
-    public Key getKey() {
-        return key;
+        return key.getItem();
     }
 }
